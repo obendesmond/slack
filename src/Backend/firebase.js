ee3973b0c778a81286ca9edcb292c8c03ac9187d
obendesmond <obendesmond2@gmail.com>
2022-03-01 13:55:12 +0100

    slack clone initialization

---
+++ src/Backend/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATE8L2PuwTbQkSj6OBLbNEzBwIjte8qh0",
+  authDomain: "slack-ca739.firebaseapp.com",
+  projectId: "slack-ca739",
+  storageBucket: "slack-ca739.appspot.com",
+  messagingSenderId: "461548883831",
+  appId: "1:461548883831:web:04183a44860c224ad69b72",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
